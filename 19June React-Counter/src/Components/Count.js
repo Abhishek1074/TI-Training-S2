d697ade18655d7ae2e55aa09fca14542246621e9
Abhishek1074 <abhishek.saxena02@telusinternational.com>
2024-07-05 14:50:04 +0530

    RickmortyAssignment

--- 19June React-Counter/src/Components/Count.js
+++ 19June React-Counter/src/Components/Count.js
@@ -11,17 +11,11 @@ function Count() {
   return (
     <div>
       <h1>Counter:{counter}</h1>
-      <button className='Inc' onClick={()=>handleclick(1)}>Inc by 1</button>
-      <button  className='Inc' onClick={()=>handleclick(5)}>Inc by 5</button>
-      <button  className='Inc' onClick={()=>handleclick(10)}>Inc by 10</button>
-      <button  className='Inc' onClick={()=>handleclick(15)}>Inc by 15</button>
-      <button  className='Inc' onClick={()=>handleclick(20)}>Inc by 20</button>
+      <button className='Inc' onClick={()=>handleclick(1)}>Inc </button>
+    
 
-      <button  className='Dec' onClick={()=>handleclick2(1)}>Dec by 1 </button>
-      <button className='Dec' onClick={()=>handleclick2(5)}>Dec by 5 </button>
-      <button className='Dec' onClick={()=>handleclick2(10)}>Dec by 10</button>
-      <button className='Dec' onClick={()=>handleclick2(15)}>Dec by 15</button>
-      <button className='Dec' onClick={()=>handleclick2(20)}>Dec by 20</button>
+      <button  className='Dec' onClick={()=>handleclick2(1)}>Dec </button>
+
     
     </div>
   )
